c4e278ecbdf33d63982fd3107fce1ef97f5a7185
David Nartey <dnartey@kth.se>
2022-08-27 17:30:58 +0200

    Accessing the inner pages of the listings

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Using Python's scrapy framework to crawl real estate listings
 
 # Verifying response for initial page content
 - Created hemnet.py file
-- Initial folder set up and listing page analysis
+- Initial folder set up and analysis on the listings page
+- Analyse listing structure in the html elements 
--- hemnet/hemnet/spiders/hemnet.py
+++ hemnet/hemnet/spiders/hemnet.py
@@ -1,3 +1,4 @@
+from pkg_resources import yield_lines
 from requests import Response
 import scrapy
 
@@ -7,5 +8,14 @@ class HemnetSpider(scrapy.Spider):
     start_urls = [ 'https://www.hemnet.se/bostader?item_types%5B%5D=radhus&item_types%5B%5D=bostadsratt']
 
     def parse(self, response):
+        
+        #Loop through listings to get ad data
+        for advert in response.css("ul.normal-results > li.normal-results__hit > a::attr('href')"):
+            yield scrapy.Request(url=advert.get(), callback=self.parseInnerPage)
+
+            # Sending the extracted links to another parse method
+            #Request a new page
+    def parseInnerPage(self, response):
         print(response.text)
+        
         
